610c3fed751bb5f3f602ab4fa19c6ecb1c3f83b8
DavePearce <david.pearce@ecs.vuw.ac.nz>
2021-11-08 13:53:02 +1300

    Update index_of to use rmc::* methods

--- src/index_of.rs
+++ src/index_of.rs
@@ -8,28 +8,21 @@ fn indexof(items: &[u32], item: u32) -> Option<usize> {
     return None;
 }
 
-fn __nondet<T>() -> T {
-    unimplemented!()
-}
-fn __VERIFIER_assume(cond: bool) {
-    unimplemented!()
-}
-
 const LIMIT : usize = 3;
 
 #[cfg(rmc)]
 #[no_mangle]
 pub fn test_01() {
     // Create arbitrary array
-    let xs : [u32; LIMIT] = __nondet();
+    let xs : [u32; LIMIT] = rmc::nondet();
     // Create arbitrary element
-    let x : u32 = __nondet();
+    let x : u32 = rmc::nondet();
     // Create arbitrary (valid) length
-    let len : usize = __nondet();
-    __VERIFIER_assume(len <= LIMIT);
+    let len : usize = rmc::nondet();
+    rmc::assume(len <= LIMIT);
     // Ensure element not in array below len
     for i in 0..len {
-	__VERIFIER_assume(xs[i] != x);
+	rmc::assume(xs[i] != x);
     }  
     // Check
     assert!(indexof(&xs[..len],x) == None);
@@ -39,20 +32,20 @@ pub fn test_01() {
 #[no_mangle]
 pub fn test_02() {
     // Create arbitrary array
-    let xs : [u32; LIMIT] = __nondet();
+    let xs : [u32; LIMIT] = rmc::nondet();
     // Create arbitrary element
-    let x : u32 = __nondet();
+    let x : u32 = rmc::nondet();
     // Create arbitrary (valid) length
-    let len : usize = __nondet();
-    __VERIFIER_assume(len <= LIMIT);
+    let len : usize = rmc::nondet();
+    rmc::assume(len <= LIMIT);
     // Create arbitrary index within array
-    let i : usize = __nondet();
-    __VERIFIER_assume(i < len);
+    let i : usize = rmc::nondet();
+    rmc::assume(i < len);
     // Ensure known value at index i
-    __VERIFIER_assume(xs[i] == x);    
+    rmc::assume(xs[i] == x);    
     // Ensure nothing in array below i matches
     for j in 0..i {
-	__VERIFIER_assume(xs[j] != x);
+	rmc::assume(xs[j] != x);
     }
     // Check find correct one
     assert!(indexof(&xs[..len],x) == Some(i));
